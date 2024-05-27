cfb6daa9beea482d29cd2612e8b5e8b953d8475d
vincenteof <14847208+vincenteof@users.noreply.github.com>
2024-05-27 22:19:55 +0800

    feat: basic pair contract

---
+++ packages/core/contracts/MinDexV2Pair.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: ISC
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+
+contract MinDexSwapV2Pair is ERC20 {
+    using Math for uint256;
+
+    uint256 constant MINIMUM_LIQUIDITY = 1000;
+    address public token0;
+    address public token1;
+
+    uint256 private reserve0;
+    uint256 private reserve1;
+
+    constructor(
+        address _token0,
+        address _token1
+    ) ERC20("MinDexSwapV2Pair", "MDSV2P") {
+        token0 = _token0;
+        token1 = _token1;
+    }
+
+    function mint() public {
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(token1).balanceOf(address(this));
+        uint256 amount0 = balance0 - reserve0;
+        uint256 amount1 = balance1 - reserve0;
+        uint256 liquidity;
+        uint256 totalSupply = totalSupply();
+        if (totalSupply == 0) {
+            liquidity = Math.sqrt(amount0 * amount1) - MINIMUM_LIQUIDITY;
+            _mint(address(0), MINIMUM_LIQUIDITY);
+        } else {
+            liquidity = Math.min(
+                (amount0 * totalSupply) / reserve0,
+                (amount1 * totalSupply) / reserve1
+            );
+        }
+
+        if (liquidity <= 0) {
+            // todo: revert with error
+        }
+
+        _mint(msg.sender, liquidity);
+        _update(balance0, balance1);
+
+        // todo: emit event
+    }
+
+    function burn() public {
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(token1).balanceOf(address(this));
+        uint256 liquidity = balanceOf(msg.sender);
+
+        uint256 totalSupply = totalSupply();
+        uint256 amount0 = (liquidity * balance0) / totalSupply;
+        uint256 amount1 = (liquidity * balance1) / totalSupply;
+
+        if (amount0 <= 0 || amount1 <= 1) {
+            // todo: revert with error
+        }
+
+        _burn(msg.sender, liquidity);
+        _safeTransfer(token0, msg.sender, amount0);
+        _safeTransfer(token1, msg.sender, amount1);
+
+        balance0 = IERC20(token0).balanceOf(address(this));
+        balance1 = IERC20(token1).balanceOf(address(this));
+
+        _update(balance0, balance1);
+
+        // todo: emit events
+    }
+
+    function _update(uint256 balance0, uint256 balance1) private {
+        reserve0 = balance0;
+        reserve1 = balance1;
+        // todo: emit sync event
+    }
+
+    function _safeTransfer(address token, address to, uint256 value) private {
+        bool success = IERC20(token).transfer(to, value);
+        if (!success) {
+            // todo: revert with error
+        }
+    }
+}
